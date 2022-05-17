85bc735c378717e6e37384b2360161d7792a5b30
Melisa Ortiz <sofimeli.ortiz@gmail.com>
2022-05-16 22:10:47 -0300

    proyecto inicial

---
+++ ProyectoFinal/src/main/java/com/madi/proyecto_final/ProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.madi.proyecto_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalApplication.class, args);
+	}
+
+}
